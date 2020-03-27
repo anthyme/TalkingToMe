d370b49c7773828dcb520f1c389f05832d081eb3
Louis Ecuvillon <louis.ecuvillon@viseo.com>
2020-03-27 18:02:15 +0100

    Putting the build folder in git

---
+++ src/frontend/talkingtomefront/build/precache-manifest.e9f1ca0ee26be8a923961b3552a6fd9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfde1c34467a6ecd2a1173ddb8adf0bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a53e894782547ebc6255",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "519ab30a54ac5f633503",
+    "url": "/static/js/2.32452701.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.32452701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a53e894782547ebc6255",
+    "url": "/static/js/main.161e9bc8.chunk.js"
+  },
+  {
+    "revision": "b638050ad95c8d5631b4",
+    "url": "/static/js/runtime-main.7afc5618.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
