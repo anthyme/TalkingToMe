49993f9f38d3441c9f5b1a79be5ad56fb416f866
Louis Ecuvillon <louis.ecuvillon@viseo.com>
2020-03-27 18:05:02 +0100

    Ajout de build a git et retrait du deploy du build

--- src/frontend/talkingtomefront/src/App.js
+++ src/frontend/talkingtomefront/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>Hello this Mael and Louis for a second test sorry money</div>
+        <div>Hello this Mael and Louis for a last, should be gould inch dieu</div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
