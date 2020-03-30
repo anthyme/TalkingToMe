6833e4281b1835ffb0d595a1ebca4719079eff8c
Maeltissot <tissotm@hotmail.com>
2020-03-30 15:10:52 +0200

    test

---
+++ src/backend/App/BaseMigration/Context/BaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.BaseMigration.Context
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        { }
+
+        public DbSet<HelloWorld> HelloWorldBase { get; set; }
+    }
+}
--- src/backend/App/Startup.cs
+++ src/backend/App/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using App.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace App
 {
@@ -25,6 +20,9 @@ namespace App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<BaseContext>(options =>
+           options.UseSqlServer(Configuration.GetConnectionString("DBString")));
             services.AddControllers();
         }
 
@@ -35,6 +33,7 @@ namespace App
             {
                 app.UseDeveloperExceptionPage();
             }
+          
 
             app.UseHttpsRedirection();
 
