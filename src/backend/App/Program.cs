29dee3b70a053a1a0d1a7d5c06e78111f19d3dff
Maeltissot <tissotm@hotmail.com>
2020-03-30 16:09:19 +0200

    push

--- src/backend/App/Program.cs
+++ src/backend/App/Program.cs
@@ -18,7 +18,7 @@ namespace App
           
 
             CreateHostBuilder(args).Build().Run();
-        }
+        }//
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
