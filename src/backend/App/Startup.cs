ae1652ee438e50311bd7f5bb3dddb2bd1795fce9
Louis Ecuvillon <louis.ecuvillon@viseo.com>
2020-03-30 17:35:39 +0200

    Fecth HellowWorld

--- src/backend/App/Startup.cs
+++ src/backend/App/Startup.cs
@@ -20,7 +20,13 @@ namespace App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors(o => o.AddPolicy("ReactPolicy", builder =>
+            {
+                builder.WithOrigins("http://localhost:3000")
+                       .AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .AllowCredentials();
+            }));
             services.AddDbContext<BaseContext>(options =>
            options.UseSqlServer(Configuration.GetConnectionString("DBString")));
             services.AddControllers();
@@ -40,12 +46,15 @@ namespace App
 
             app.UseRouting();
 
+            app.UseCors("ReactPolicy");
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
         }
 
     }
