7e33245c468ea1e06277390150566e22eea6bddc
Maeltissot <tissotm@hotmail.com>
2020-04-07 17:15:13 +0200

    added dispatcher

---
+++ src/frontend/talkingtomefront/src/store/MainDispatcher.tsx
@@ -0,0 +1,19 @@
+import { Dispatch } from "react";
+import { DispatchAction } from "./reducers/MainReducer";
+import * as actions from "./ActionsTypes";
+
+export class RootDispatcher {
+    
+    private readonly dispatch: Dispatch<DispatchAction>;
+    
+    constructor(dispatch: Dispatch<DispatchAction>){
+        this.dispatch = dispatch; 
+    }
+    setAnswerIdRdx = (currentAnswerIdRdx: number) => this.dispatch({type: actions.UPDATE_CURRENTANSWERID_VALUE, payload: {currentAnswerIdRdx}});
+    
+    setAnswerRdx = (currentAnswerRdx: string) => this.dispatch({type: actions.UPDATE_CURRENTANSWER_VALUE, payload: {currentAnswerRdx}});
+    
+    setQuestionIdRdx = (questionIdRdx: number) => this.dispatch({type: actions.UPDATE_QUESTIONID_VALUE, payload: {questionIdRdx}});
+    
+    setQuestionRdx = (questionRdx :Object) => this.dispatch({type: actions.UPDATE_QUESTION_VALUE, payload: {questionRdx}})
+}
