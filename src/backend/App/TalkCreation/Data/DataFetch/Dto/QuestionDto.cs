cdf109125381733794125d46b8c0454aa594b4d6
Maeltissot <tissotm@hotmail.com>
2020-04-23 18:14:31 +0200

    QuestionDTo added

---
+++ src/backend/App/TalkCreation/Data/DataFetch/Dto/QuestionDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.TalkCreation.Data.DataFetch.Dto
+{
+    public class QuestionDto
+    {
+        public int Id { get; set; }
+        public List<string> Answers { get; set; }
+        public string CorrectAn { get; set; }
+        public string Type { get; set; }
+        public string Question { get; set; }
+    }
+}
