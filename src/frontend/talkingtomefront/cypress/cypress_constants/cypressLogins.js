c1608f973e1d840c193dbe5f66c434e9c3e4f819
Louis Ecuvillon <louis.ecuvillon@viseo.com>
2020-05-20 11:41:26 +0200

    Test gitignore cypress

---
+++ src/frontend/talkingtomefront/cypress/cypress_constants/cypressLogins.js
@@ -0,0 +1,4 @@
+//THIS FILE MUST ALWAYS BE GIT IGNORED //
+
+export const urlDataBase = 'https://localhost:44381/api/'; //'https://talkingtome-api.azurewebsites.net/api/';
+export const urlHub = 'https://localhost:44381/'; //'https://talkingtome-api.azurewebsites.net/';
