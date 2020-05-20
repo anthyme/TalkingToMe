c31d34717279301accc6645541e39f1dd7ef0ad1
Louis Ecuvillon <louis.ecuvillon@viseo.com>
2020-05-20 11:43:26 +0200

    Raté

--- src/frontend/talkingtomefront/cypress/cypress_constants/cypressLogins.js
+++
@@ -1,4 +0,0 @@
-//THIS FILE MUST ALWAYS BE GIT IGNORED //
-
-export const urlDataBase = 'https://localhost:44381/api/'; //'https://talkingtome-api.azurewebsites.net/api/';
-export const urlHub = 'https://localhost:44381/'; //'https://talkingtome-api.azurewebsites.net/';
