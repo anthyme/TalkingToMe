adafba471b53f8f5aae6d3cec31427e077555030
Maeltissot <tissotm@hotmail.com>
2020-05-28 11:47:47 +0200

    chat with autoscroll

--- src/frontend/talkingtomefront/src/chatBox/ChatInterface.tsx
+++ src/frontend/talkingtomefront/src/chatBox/ChatInterface.tsx
@@ -1,13 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import Divider from '@material-ui/core/Divider'
-import ListItemText from '@material-ui/core/ListItemText'
-import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import Avatar from '@material-ui/core/Avatar'
-import Typography from '@material-ui/core/Typography'
-import { Button, TextField } from '@material-ui/core'
+import SendIcon from '@material-ui/icons/Send'
+import { Button, TextField, IconButton } from '@material-ui/core'
 import Message from './Message'
 
 const useStyles = makeStyles((theme) => ({
@@ -15,17 +10,20 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     maxWidth: '70%',
     backgroundColor: theme.palette.background.paper,
-
   },
   bottom: {
-    position: "absolute", 
-    bottom: "5px",
-},
+    position: 'absolute',
+    bottom: '5px',
+  },
   inline: {
     display: 'center',
   },
   overflow: {
-    overflow: "auto",
+    overflowY: "scroll",
+    height: "545px",
+  },
+  textArea: {
+    resize: "none",
   },
 }))
 interface IProps {
@@ -47,7 +45,7 @@ const ChatInterface: React.FC<IProps> = (props) => {
     if (connection) {
       connection.invoke('PostQuestion', groupId, question, userName)
     }
-    setQuestion('');
+    setQuestion('')
   }
   const handleQuestionChange = (event: any) => {
     setQuestion(event.target.value)
@@ -56,29 +54,37 @@ const ChatInterface: React.FC<IProps> = (props) => {
     setUserName(event.target.value)
   }
   useEffect(() => {
-    connection.invoke("GetCurrentSessionUserQuestions", groupId);
+    connection.invoke('GetCurrentSessionUserQuestions', groupId)
     connection.on('AddNewQuestion', async (result: any) => {
-      if(messages[0]==={}){
-        await setMessages([result]);
-      } else{
-        let newQuestionList=messages;
-        newQuestionList.push(result);
-        console.log(newQuestionList)
-        await setMessages([...messages]);
+      if (messages[0] === {}) {
+        await setMessages([result])
+      } else {
+        let newQuestionList = messages
+        newQuestionList.push(result)
+        await setMessages([...messages])
       }
-    });
-    connection.on('ShowCurrentUserQuestions', async (results: any) => {
-        let newQuestionList=messages;
-        console.log(results)
-        if(results!==null){
-          results.forEach((element: Object) => {
-            newQuestionList.push(element);
-          });
-          console.log(newQuestionList)
-          await setMessages([...messages]);
+      const scrollMessages = document.getElementById('scrollMessages');
+      if(scrollMessages!==null){
+        const shouldScroll = scrollMessages.scrollTop + scrollMessages.clientHeight >= scrollMessages.scrollHeight-100;
+        if(shouldScroll){
+          scrollMessages.scrollTop = scrollMessages.scrollHeight;
         }
+      }
     })
-    
+    connection.on('ShowCurrentUserQuestions', async (results: any) => {
+      let newQuestionList = messages
+      console.log(results)
+      if (results !== null) {
+        results.forEach((element: Object) => {
+          newQuestionList.push(element)
+        })
+        await setMessages([...messages])
+      }
+      const scrollMessages = document.getElementById('scrollMessages');
+      if(scrollMessages!==null){
+        scrollMessages.scrollTop = scrollMessages.scrollHeight;
+      }
+    })
   }, []) //Load only on
   return (
     <div>
@@ -91,17 +97,26 @@ const ChatInterface: React.FC<IProps> = (props) => {
         autoComplete="fname"
         onChange={handleUserNameChange}
       />
-      <div className={classes.overflow}>
-      <List className={classes.root}>
-      {messages.length!==1 ? (messages.map(
-              (message: any) =>
-                  <Message connection={connection} message={message} likedQuestions={props.likedQuestions} changeLikedQuestions={props.changeLikedQuestions}/>
-            )) : <></>}
-      </List>
+      <div className={classes.overflow} id="scrollMessages">
+        <List className={classes.root}>
+          {messages.length !== 1 ? (
+            messages.map((message: any) => (
+              <Message
+                connection={connection}
+                message={message}
+                likedQuestions={props.likedQuestions}
+                changeLikedQuestions={props.changeLikedQuestions}
+              />
+            ))
+          ) : (
+            <></>
+          )}
+        </List>
       </div>
       <div className={classes.bottom}>
         <textarea
-          rows={4}
+          className={classes.textArea}
+          rows={2}
           cols={50}
           name="comment"
           form="usrform"
@@ -109,7 +124,13 @@ const ChatInterface: React.FC<IProps> = (props) => {
           onChange={handleQuestionChange}
           placeholder="Enter text here..."
         />
-        <Button onClick={SendNewQuestions}>Send</Button>
+        <IconButton onClick={SendNewQuestions} className="tooltiptext" disabled={question===""}>
+          {question ? (
+            <SendIcon color="primary" fontSize="large" />
+          ) : (
+            <SendIcon color="disabled" fontSize="large"/>
+          )}
+        </IconButton>
       </div>
     </div>
   )
