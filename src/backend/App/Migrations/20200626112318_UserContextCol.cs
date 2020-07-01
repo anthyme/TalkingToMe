eca4e0945ba7dc3e35c692506c3d749bfd82c0b3
Maeltissot <tissotm@hotmail.com>
2020-07-01 15:06:06 +0200

    added sessions, mute, timers

---
+++ src/backend/App/Migrations/20200626112318_UserContextCol.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Migrations
+{
+    public partial class UserContextCol : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserContext",
+                table: "UserQuestions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserContext",
+                table: "UserQuestions");
+        }
+    }
+}
